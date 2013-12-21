036e10ff82ec4b2c380d13fabb3c32c606c0bcc2
Sebastien Badia <seb@sebian.fr>
2013-12-21 22:31:44 +0100

    spec: do not redeclare params in spec files

--- spec/classes/metche_spec.rb
+++ spec/classes/metche_spec.rb
@@ -2,6 +2,20 @@ require 'spec_helper'
 
 describe 'metche', :type => :class do
   let(:title) { 'metche' }
+
+  let :params_set do
+    {
+      :changelog    => '/home/foobar/changelog',
+      :watchdir     => '/etc',
+      :dodetail     => 'yes',
+      :test_time    => '42',
+      :stable_time  => '12',
+      :email        => 'foobar@fooboozoo.fr',
+      :send_stable  => 'yes',
+      :excludes     => 'git',
+    }
+  end
+
   let(:params) {{
     :owner => 'root',
   }}
@@ -21,27 +35,16 @@ describe 'metche', :type => :class do
   end
 
   context 'Custom params' do
-    let(:params) do
-      {
-      :changelog    => '/home/foobar/changelog',
-      :watchdir     => '/etc',
-      :dodetail     => 'yes',
-      :test_time    => '42',
-      :stable_time  => '12',
-      :email        => 'foobar@fooboozoo.fr',
-      :send_stable  => 'yes',
-      :excludes     => '*.git',
-      }
-    end
+    let(:params) { params_set }
     it 'Custom metche params' do
-      should contain_file('/etc/metche.conf').with_content(/CHANGELOG_FILE="\/home\/foobar\/changelog"/)
-      should contain_file('/etc/metche.conf').with_content(/WATCHED_DIR="\/etc"/)
-      should contain_file('/etc/metche.conf').with_content(/DO_DETAILS="yes"/)
-      should contain_file('/etc/metche.conf').with_content(/TESTING_TIME="42"/)
-      should contain_file('/etc/metche.conf').with_content(/STABLE_TIME="12"/)
-      should contain_file('/etc/metche.conf').with_content(/EMAIL_ADDRESS="foobar@fooboozoo\.fr"/)
-      should contain_file('/etc/metche.conf').with_content(/SEND_STABLE_REPORTS="yes"/)
-      should contain_file('/etc/metche.conf').with_content(/EXCLUDES="\*.git"/)
+      should contain_file('/etc/metche.conf').with_content(/CHANGELOG_FILE="#{params_set[:changelog]}"/)
+      should contain_file('/etc/metche.conf').with_content(/WATCHED_DIR="#{params_set[:watchdir]}"/)
+      should contain_file('/etc/metche.conf').with_content(/DO_DETAILS="#{params_set[:dodetail]}"/)
+      should contain_file('/etc/metche.conf').with_content(/TESTING_TIME="#{params_set[:test_time]}"/)
+      should contain_file('/etc/metche.conf').with_content(/STABLE_TIME="#{params_set[:stable_time]}"/)
+      should contain_file('/etc/metche.conf').with_content(/EMAIL_ADDRESS="#{params_set[:email]}"/)
+      should contain_file('/etc/metche.conf').with_content(/SEND_STABLE_REPORTS="#{params_set[:send_stable]}"/)
+      should contain_file('/etc/metche.conf').with_content(/EXCLUDES="#{params_set[:excludes]}"/)
     end
   end
 
